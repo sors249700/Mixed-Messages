966408b1b53e65f2fe807555290e2925fbd1500d
Sors <113908179+sors249700@users.noreply.github.com>
2022-10-11 15:03:46 +0200

    Update mixedmessages.js

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,8 +1,8 @@
 const Randomize = () => {
     //  ingredients
-    let main = ['noodles', 'rice', 'fries', 'spaghetti', 'dumplings', 'potatoes', ];
-    let garnish = ['sausage', 'steak', 'beans', 'peas', 'spinach', 'fish', 'chicken', 'eggs', 'broccoli', ];
-    let sauce = ['gravy', 'cream sauce', 'curry sauce', 'barbecue sauce', 'ketchup', 'mayonnaise', ];
+    let main = ['noodles', 'rice', 'fries', 'spaghetti', 'dumplings', 'potatoes' ];
+    let garnish = ['sausage', 'steak', 'beans', 'peas', 'spinach', 'fish', 'chicken', 'eggs', 'broccoli' ];
+    let sauce = ['gravy', 'cream sauce', 'curry sauce', 'barbecue sauce', 'ketchup', 'mayonnaise' ];
 
     // make my ingredients random.
     let randomMain = Math.floor(Math.random() * main.length);
